5c188ff22d0080e73f0b9024dea3e2d14b795704
z017 <jeremiaslongo@gmail.com>
2015-12-08 21:01:51 -0300

    Add functions for template parsing

--- common.sh
+++ common.sh
@@ -28,3 +28,39 @@ function required {
   done
   [[ $e < 1 ]] || exit 2
 }
+
+# Parse template file variables in the format "{{ VAR }}" with the "VAR" value.
+# parse_template <input file template> <output file> <string of variables>
+function parse_template {
+  local e=0
+  [[ ! -f "$1" ]] && err "$1 is not a valid file." && e=1
+  [[ $2 != ${2%/*} ]] && mkdir -p ${2%/*}
+  [[ -z $3 ]] && err "$3, must be an string of variables to replace" && e=1
+  if [[ $e > 0 ]]; then
+    ech "Usage: parse_template <input file template> <output file> <string of variables>"
+    exit 2
+  fi
+  local args
+  for v in $3; do
+    args="${args}s~{{ $v }}~${!v}~g;"
+  done
+  sed "$args" < $1 > $2
+}
+
+# Parse all template files ".tpl" in the input_dir and saved them to output_dir
+# parse_templates <input_dir> <output_dir> <string of variables>
+function parse_templates {
+  local e=0
+  [[ ! -d $1 ]] && err "$1 is not a valid directory." && e=1
+  [[ -z $3 ]] && err "$3, must be an string of variables to replace" && e=1
+  if [[ $e > 0 ]]; then
+    ech "Usage: parse_templates <input_dir> <output_dir> <string of variables>"
+    exit 2
+  fi
+  # parse each file
+  for file in "$1"/*.tpl*; do
+    local filename=${file##*/}
+    local outfile=${filename%.tpl*}${filename##*.tpl}
+    parse_template $file $2/$outfile "$3"
+  done
+}
